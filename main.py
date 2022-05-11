46becc0f147177f2e7db97d705665929108dd295
Yaroslav Novitsky <rcr3211@gmail.com>
2022-05-11 14:29:03 +0300

    hotfix: requests; add: nixie theme

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ async def get_root():
     return FileResponse("index.html", media_type="text/html")
 
 
-@app.get("/request/{number}")
-async def get_number(number: int, max_length: int = 7, lead_zeros: bool = False,
+@app.get("/request/number")
+async def get_number(value: int, max_length: int = 7, lead_zeros: bool = False,
                      theme: str = None):
-    return HTMLResponse(theme_manager.build_image(number, max_length, False, lead_zeros, theme),
+    return HTMLResponse(theme_manager.build_image(value, max_length, False, lead_zeros, theme),
                         media_type="image/svg+xml", headers={'cache-control': 'max-age=31536000'})
 
 
